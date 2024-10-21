5704c99a0fe47db6f09fa590e7d65b664eba3b5e
Cartaphilus <49419636+DhiaUlhaq0@users.noreply.github.com>
2024-10-21 15:39:19 +0700

    git-init

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "hello world"
+    })
+})
+
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: "not found"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`server strated, list on port ${PORT}`);
+})
